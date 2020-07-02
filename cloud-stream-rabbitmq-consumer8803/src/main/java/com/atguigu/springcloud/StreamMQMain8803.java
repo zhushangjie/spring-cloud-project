581638041d57ea823d0685f43ce1817fceb346c0
zhushangjie <2818918708@qq.com>
2020-07-02 22:26:10 +0800

    springcloud stream 之消息的重复消费问题

---
+++ cloud-stream-rabbitmq-consumer8803/src/main/java/com/atguigu/springcloud/StreamMQMain8803.java
@@ -0,0 +1,18 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zhushangjie
+ * @version 1.0
+ * @date 2020/7/2 22:04
+ */
+
+@SpringBootApplication
+public class StreamMQMain8803 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StreamMQMain8803.class, args);
+    }
+}
